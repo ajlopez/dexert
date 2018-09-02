6908e6003d7b2bef68b8656d38d37df8a95a6fcf
ajlopez <webmaster@ajlopez.com>
2018-09-02 17:39:50 -0300

    Refactor to use Balance structure with available amount

--- contracts/Dexert.sol
+++ contracts/Dexert.sol
@@ -3,42 +3,48 @@ pragma solidity ^0.4.24;
 import "./ERC20.sol";
 
 contract Dexert {
-    mapping (address => uint) private balances;
-    mapping (address => mapping (address => uint)) private tokenBalances;
+    struct Balance {
+        uint available;
+    }
+    
+    mapping (address => Balance) private balances;
+    mapping (address => mapping (address => Balance)) private tokenBalances;
     
     function deposit() payable public {
-        balances[msg.sender] += msg.value;
+        balances[msg.sender].available += msg.value;
     }
     
     function withdraw(uint amount) public returns (bool) {
-        require(balances[msg.sender] >= amount);
-        balances[msg.sender] -= amount;
+        require(balances[msg.sender].available >= amount);
+        
+        balances[msg.sender].available -= amount;
         msg.sender.transfer(amount);
+        
         return true;
     }
     
     function getBalance(address addr) public view returns (uint) {
-        return balances[addr];
+        return balances[addr].available;
     }
 
     function depositTokens(ERC20 token, uint amount) public returns (bool) {
         require(token.transferFrom(msg.sender, this, amount));
         
-        tokenBalances[address(token)][msg.sender] += amount;
+        tokenBalances[address(token)][msg.sender].available += amount;
         
         return true;
     }
 
     function withdrawTokens(ERC20 token, uint amount) public returns (bool) {
-        require(tokenBalances[address(token)][msg.sender] >= amount);
+        require(tokenBalances[address(token)][msg.sender].available >= amount);
         require(token.transfer(msg.sender, amount));
         
-        tokenBalances[address(token)][msg.sender] -= amount;
+        tokenBalances[address(token)][msg.sender].available -= amount;
         
         return true;
     }
     
     function getTokenBalance(address token, address addr) public view returns (uint) {
-        return tokenBalances[token][addr];
+        return tokenBalances[token][addr].available;
     }
 }
